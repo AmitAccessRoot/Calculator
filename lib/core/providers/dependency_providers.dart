39fb3051f263e938f17830150582b466de4b7af6
AmitAccessRoot <ak22682296@gmail.com>
2026-08-03 10:41:54 +0530

    Implement dependency providers for history features
    Added providers for local data source, repository, and use cases related to history management.

---
+++ lib/core/providers/dependency_providers.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../data/datasources/local/history_local_datasource.dart';
+import '../../data/repositories/history_repository_impl.dart';
+import '../../domain/repositories/history_repository.dart';
+import '../../domain/usecases/clear_history_usecase.dart';
+import '../../domain/usecases/get_history_usecase.dart';
+import '../../domain/usecases/save_calculation_usecase.dart';
+
+// Data Source Provider
+final historyLocalDataSourceProvider = Provider<HistoryLocalDataSource>((ref) {
+  return HistoryLocalDataSourceImpl();
+});
+
+// Repository Provider
+final historyRepositoryProvider = Provider<HistoryRepository>((ref) {
+  final localDataSource = ref.read(historyLocalDataSourceProvider);
+  return HistoryRepositoryImpl(localDataSource);
+});
+
+// Usecases Providers
+final saveCalculationUseCaseProvider = Provider<SaveCalculationUseCase>((ref) {
+  return SaveCalculationUseCase(ref.read(historyRepositoryProvider));
+});
+
+final getHistoryUseCaseProvider = Provider<GetHistoryUseCase>((ref) {
+  return GetHistoryUseCase(ref.read(historyRepositoryProvider));
+});
+
+final clearHistoryUseCaseProvider = Provider<ClearHistoryUseCase>((ref) {
+  return ClearHistoryUseCase(ref.read(historyRepositoryProvider));
+});
