5a2ca2729ef811d67eae975f181687e8344f81dc
AmitAccessRoot <ak22682296@gmail.com>
2026-08-03 11:33:50 +0530

    Add deleteCalculationUseCaseProvider to dependencies

--- lib/core/providers/dependency_providers.dart
+++ lib/core/providers/dependency_providers.dart
@@ -1,24 +1,22 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 import '../../data/datasources/local/history_local_datasource.dart';
 import '../../data/repositories/history_repository_impl.dart';
 import '../../domain/repositories/history_repository.dart';
 import '../../domain/usecases/clear_history_usecase.dart';
 import '../../domain/usecases/get_history_usecase.dart';
 import '../../domain/usecases/save_calculation_usecase.dart';
+import '../../domain/usecases/delete_calculation_usecase.dart';
 
-// Data Source Provider
+// Providers
 final historyLocalDataSourceProvider = Provider<HistoryLocalDataSource>((ref) {
   return HistoryLocalDataSourceImpl();
 });
 
-// Repository Provider
 final historyRepositoryProvider = Provider<HistoryRepository>((ref) {
   final localDataSource = ref.read(historyLocalDataSourceProvider);
   return HistoryRepositoryImpl(localDataSource);
 });
 
-// Usecases Providers
 final saveCalculationUseCaseProvider = Provider<SaveCalculationUseCase>((ref) {
   return SaveCalculationUseCase(ref.read(historyRepositoryProvider));
 });
@@ -30,11 +28,6 @@ final getHistoryUseCaseProvider = Provider<GetHistoryUseCase>((ref) {
 final clearHistoryUseCaseProvider = Provider<ClearHistoryUseCase>((ref) {
   return ClearHistoryUseCase(ref.read(historyRepositoryProvider));
 });
-
-// (Keep all previous code as it is in dependency_providers.dart)
-// Add this new provider at the end:
-
-import '../../domain/usecases/delete_calculation_usecase.dart';
 
 final deleteCalculationUseCaseProvider = Provider<DeleteCalculationUseCase>((ref) {
   return DeleteCalculationUseCase(ref.read(historyRepositoryProvider));
