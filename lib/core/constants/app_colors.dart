35fc1a5d89a0825f7eebcfaed682d9b7e82b5164
AmitAccessRoot <ak22682296@gmail.com>
2026-08-03 09:35:23 +0530

    Add AppColors class for theme color constants

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  const AppColors._();
+
+  // Light Theme Colors
+  static const Color lightBackground = Color(0xFFF3F4F6);
+  static const Color lightSurface = Color(0xFFFFFFFF);
+  static const Color lightPrimary = Color(0xFF3B82F6);
+  static const Color lightTextPrimary = Color(0xFF111827);
+  static const Color lightTextSecondary = Color(0xFF6B7280);
+  static const Color lightError = Color(0xFFEF4444);
+  static const Color lightDivider = Color(0xFFE5E7EB);
+
+  // Dark Theme Colors
+  static const Color darkBackground = Color(0xFF111827);
+  static const Color darkSurface = Color(0xFF1F2937);
+  static const Color darkPrimary = Color(0xFF60A5FA);
+  static const Color darkTextPrimary = Color(0xFFF9FAFB);
+  static const Color darkTextSecondary = Color(0xFF9CA3AF);
+  static const Color darkError = Color(0xFFF87171);
+  static const Color darkDivider = Color(0xFF374151);
+
+  // Operator Colors
+  static const Color operatorColor = Color(0xFFF59E0B);
+  static const Color equalButtonColor = Color(0xFF10B981);
+}
